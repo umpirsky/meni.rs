5a21b2e59941256ba59b5b6d723571392f9ed774
umpirsky <umpirsky@gmail.com>
2015-03-28 19:14:37 +0100

    Restaurant list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,5 +4,8 @@
  * @name meniApp.controller:MainCtrl
  */
 angular.module('meniApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('restaurants/restaurants.json').success(function(data) {
+      $scope.restaurants = data;
+    });
   });
