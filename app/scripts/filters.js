044dbe7b1e1d5809c31470c001da798166cfbd8c
umpirsky <umpirsky@gmail.com>
2015-04-11 20:23:19 +0200

    Extend data model

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -1,7 +1,37 @@
 'use strict';
 
-angular.module('restaurantsFilters', []).filter('thumbnail', function() {
-  return function(url) {
-    return url.replace('images/', 'images/thumbnails/');
-  };
-});
+angular.module('restaurantsFilters', [])
+  .filter('thumbnail', function() {
+    return function(url) {
+      return url.replace('images/', 'images/thumbnails/');
+    };
+  })
+  .filter('urlname', function() {
+    return function(url) {
+      var link = document.createElement('a');
+      link.href = url;
+
+      if (-1 !== link.href.indexOf('facebook.com/pages')) {
+        return link.pathname.split('/')[2];
+      }
+
+      if (-1 !== link.hostname.indexOf('facebook')) {
+        return link.pathname.replace(/\//, '');
+      }
+
+      return link.hostname.replace('www.', '');
+    };
+  })
+  .filter('urlicon', function() {
+    return function(url) {
+      var link = document.createElement('a');
+      link.href = url;
+
+      if (-1 !== link.hostname.indexOf('facebook')) {
+        return 'facebook';
+      }
+
+      return 'link';
+    };
+  })
+;
