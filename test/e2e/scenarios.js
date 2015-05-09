33e6d110f7d917f9eb961a6ceb7a693ec12c97ee
umpirsky <umpirsky@gmail.com>
2015-05-09 22:29:29 +0200

    Fix tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -14,14 +14,14 @@ describe('Restaurants App', function() {
       var restaurantList = element.all(by.repeater('restaurant in restaurants'));
       var query = element(by.model('query'));
 
-      expect(restaurantList.count()).toBe(14);
+      expect(restaurantList.count()).toBe(17);
 
       query.sendKeys('Cezar');
       expect(restaurantList.count()).toBe(1);
 
       query.clear();
       query.sendKeys('pica');
-      expect(restaurantList.count()).toBe(8);
+      expect(restaurantList.count()).toBe(9);
     });
   });
 });
