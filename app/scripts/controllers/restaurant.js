d99129b8997c087a4bd4c63e35a0f9f1504edaf7
umpirsky <umpirsky@gmail.com>
2015-05-09 23:27:07 +0200

    Move breakpoints to js

--- app/scripts/controllers/restaurant.js
+++ app/scripts/controllers/restaurant.js
@@ -17,4 +17,24 @@ angular.module('restaurantsApp')
         scope: $scope
       });
     };
+
+    $scope.breakpoints = [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }];
   });
