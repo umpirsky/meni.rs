0ef6e71d9d209421d4e4eeda37939ecd5c8d9980
umpirsky <umpirsky@gmail.com>
2015-06-07 16:44:12 +0200

    Phone url

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -6,6 +6,11 @@ angular.module('restaurantsFilters', [])
       return url.replace('images/', 'images/thumbnails-' + size + '/');
     };
   })
+  .filter('phoneUrl', function() {
+    return function(phone) {
+      return phone ? 'tel:' + phone.split(',')[0] : phone;
+    };
+  })
   .filter('urlText', ['urlText', function(urlText) {
     return function(url) {
       return urlText(url);
