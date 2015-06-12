c442391c90fd5ad2c1df04340b2eb27e1cdb5833
umpirsky <umpirsky@gmail.com>
2015-06-12 17:58:15 +0200

    Geolocation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,5 +4,21 @@
  * @name restaurantsApp.controller:MainCtrl
  */
 angular.module('restaurantsApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $location) {
+    geolocator.locate(function onGeoSuccess(geolocation) {
+      if ('RS' != geolocation.address.countryCode) {
+        return;
+      }
+
+      switch(geolocation.address.city) {
+        case 'Niš':
+          $location.path('/nis');
+          break;
+        case 'Novi Sad':
+          $location.path('/novi-sad');
+          break;
+      }
+
+      $scope.$apply();
+    });
   });
