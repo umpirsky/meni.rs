25d1ab775b1ac473f09e32a5a9f75ecb1455205d
umpirsky <umpirsky@gmail.com>
2015-06-12 14:38:34 +0200

    Simpler tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,25 +3,16 @@
 describe('Restaurants App', function() {
 
   describe('Restaurant list view', function() {
-
     beforeEach(function() {
       browser.get('/');
     });
 
-
     it('should filter restaurant list as a user types into the search box', function() {
-
       var restaurantList = element.all(by.repeater('restaurant in restaurants'));
       var query = element(by.model('query'));
 
-      expect(restaurantList.count()).toBe(21);
-
       query.sendKeys('Cezar');
       expect(restaurantList.count()).toBe(1);
-
-      query.clear();
-      query.sendKeys('pica');
-      expect(restaurantList.count()).toBe(10);
     });
   });
 });
