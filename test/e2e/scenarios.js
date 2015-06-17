9a8c5e790f6dafa3e4fbed2220b968f04c10f19b
umpirsky <umpirsky@gmail.com>
2015-06-17 09:24:47 +0200

    Fix the build

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,10 +1,26 @@
 'use strict';
 
 describe('Restaurants App', function() {
+  describe('Location list view', function() {
+    beforeEach(function() {
+      browser.get('/');
+    });
+
+    it('should list Niš and Novi Sad', function() {
+      expect(element.all(by.css('.location-item')).count()).toBe(2);
+      expect(element.all(by.css('h2')).getText()).toEqual(['Niš', 'Novi Sad'])
+    });
+  });
 
   describe('Restaurant list view', function() {
     beforeEach(function() {
-      browser.get('/');
+      browser.get('/#/nis');
+    });
+
+    it('should list restaurants', function() {
+      var restaurantList = element.all(by.repeater('restaurant in restaurants'));
+
+      expect(restaurantList.count()).toBeGreaterThan(20);
     });
 
     it('should filter restaurant list as a user types into the search box', function() {
